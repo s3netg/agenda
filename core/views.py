37407797f56d1b29b4b8987ef0c9311af1276e9e
s3netg <s3netg@gmail.com>
2020-08-25 17:24:14 -0300

    alteraçoes 3

--- core/views.py
+++ core/views.py
@@ -32,7 +32,28 @@ def index(request):
     return redirect('/agenda')
 
 @login_required(login_url='/login/')
+def evento(request):
+    return render(request,'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_do_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_do_evento,
+                              descricao=descricao,
+                              usuario=usuario )
+
+
+
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario = usuario)
     dados={'eventos':evento}
     return render(request,'agenda.html',dados)
